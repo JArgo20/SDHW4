a8db3dbe6f4cdca53150a50505abcb6089dc431f
Josiah Argo <josiah-argo@uiowa.edu>
2019-11-04 13:10:43 -0600

    Flask WTForm working

--- app.py
+++ app.py
@@ -1,28 +1,25 @@
 from flask import Flask
 from flask import render_template, request, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY']='SuperSecretKey'
+
+class JobForm(FlaskForm):
+    company_name = StringField('Company Name:', validators=[DataRequired()])
+    job_title = StringField('Job Title:', validators=[DataRequired()])
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', display="", pageTitle='Loan Calculator')
+    return render_template('index.html', pageTitle='Josiahs Job Applications')
 
-@app.route('/loan', methods=['GET', 'POST'])
-def loan():
-    if request.method == 'POST':
-        form = request.form
-        A = float(form['numOne'])
-        numPmt = float(form['numTwo'])
-        numYears = float(form['numThree'])
-        intRate = float(form['numFour'])
-        n = numPmt * numYears
-        i = (intRate / 100) / numPmt
-        D = ((( 1 + i )**n ) - 1 ) / ( i * ( 1 + i)**n )
-        calc = A / D
-        calc = (round(calc, 2))
-        return render_template('index.html', display=calc, pageTitle='Loan Calculator')
-   
-    return redirect("/")
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/add_job', methods=['GET', 'POST'])
+def add_job():
+    form = JobForm()
+    if form.validate_on_submit():
+        return "<h2> The company applied to is {0} and the job is {1}".format(form.company_name.data, form.job_title.data)
+    
+    return render_template('add_job.html', form=form, pageTitle='Add a New Job')
