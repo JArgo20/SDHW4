fe5be727e265626f451b2ac01bbb97c443332f98
Josiah Argo <josiah-argo@uiowa.edu>
2019-11-15 14:30:55 -0600

    added search function. Edited update function

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask import render_template, request, redirect, flash, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField, Form, SelectField
+from wtforms import StringField, Form, SelectField, IntegerField
 from wtforms.validators import DataRequired
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, or_
 import pymysql
 import secrets
 #import os
@@ -32,6 +32,7 @@ class jargo_jobapp(db.Model):
         return "jobId: {0} | Company Name: {1} | Job Title: {2}".format(self.id, self.company_name, self.job_title)
 
 class JobForm(FlaskForm):
+    jobId = IntegerField('Job ID:')
     company_name = StringField('Company Name:', validators=[DataRequired()])
     job_title = StringField('Job Title:', validators=[DataRequired()])
 
@@ -69,19 +70,25 @@ def update_job(jobId):
         job.company_name = form.company_name.data
         job.job_title = form.job_title.data
         db.session.commit()
-        flash('Job information has been updated.')
         return redirect(url_for('job', jobId=job.jobId))
+    
+    form.jobId.data = job.jobId
     form.company_name.data = job.company_name
     form.job_title.data = job.job_title
-    return render_template('add_job.html', form=form, pageTitle='Update Post',
+    return render_template('update_job.html', form=form, pageTitle='Update Post',
                             legend="Update A job")
 
-#@app.route('/search/<int:jobId>', methods=['GET','POST'])
-#def search_job(jobId):
-    #if request.method == 'POST': 
-        #job = jargo_jobapp.query.get_or_404(jobId)
-        #db.session.search(job)
-        #db.session.commit()
+@app.route('/search', methods=['GET','POST'])
+def search():
+    if request.method == 'POST': 
+        form = request.form
+        search_value = form['search_string']
+        search = "%{0}%".format(search_value)
+        results = jargo_jobapp.query.filter(or_(jargo_jobapp.company_name.like(search),
+                                            jargo_jobapp.job_title.like(search))).all()
+        return render_template('index.html', jobs=results, pageTitle='Josiahs jobapps', legend="Search Results")
+    else:
+        return redirect('/')
 
 @app.route('/friend/<int:jobId>/delete', methods=['POST'])
 def delete_job(jobId):
@@ -89,7 +96,6 @@ def delete_job(jobId):
         job = jargo_jobapp.query.get_or_404(jobId)
         db.session.delete(job)
         db.session.commit()
-        flash('Job was successfully deleted!')
         return redirect("/")
     else: 
         return redirect("/")
